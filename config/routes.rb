ff330e2a4de88049132935e4213e3b0a7fe4a7b5
Clement Carr <chalinecarr@gmail.com>
2023-06-14 12:40:48 +0200

    Applications Show Page and Route update

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,15 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :properties do
-    resources :property_applications, only: [:index, :new, :create] do
-      resources :bank_references, only: [:new, :show, :create, :edit]
-      resources :employment_references, only: [:new, :show, :create, :edit]
-      resources :identificiations, only: [:new, :show, :create, :edit]
-      resources :messages
-    end
+    resources :property_applications, only: [:index, :new, :create]
   end
 
-  resources :property_applications, only: [:show, :edit, :update]
+  resources :property_applications, only: [:show, :edit, :update] do
+    resources :bank_references, only: [:new, :show, :create, :edit]
+    resources :employment_references, only: [:new, :show, :create, :edit]
+    resources :identificiations, only: [:new, :show, :create, :edit]
+    resources :messages
+  end
 
   # Defines the root path route ("/")
   # root "articles#index"
