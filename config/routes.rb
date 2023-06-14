06432d2f0cdb06dce255c78fb286bf575a836bf8
YenTung Chen <cyen.tun@gmail.com>
2023-06-14 23:45:23 +0800

    push my process so far, unfinished with edit

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   resources :property_applications, only: [:show, :edit, :update] do
     resources :bank_references, only: [:new, :show, :create, :edit]
     resources :employment_documents, only: [:new, :show, :create, :edit]
-    resources :identificiations, only: [:new, :show, :create, :edit]
+    resources :identifications, only: [:new, :show, :create, :edit]
     resources :messages
   end
 
