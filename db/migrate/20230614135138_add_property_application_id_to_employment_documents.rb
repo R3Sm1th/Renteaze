a092ba07718ef7e54f7364fa73656a02e317e041
Russell Hallowes-Smith <russellsmith6@gmail.com>
2023-06-14 15:57:50 +0100

    Removed unecessary migration

--- db/migrate/20230614135138_add_property_application_id_to_employment_documents.rb
+++
@@ -1,5 +0,0 @@
-class AddPropertyApplicationIdToEmploymentDocuments < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :employment_documents, :property_application, null: false, foreign_key: true
-  end
-end
