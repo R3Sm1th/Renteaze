b6fab9da3034eecca7292568a78c0c2649a28609
Clement Carr <chalinecarr@gmail.com>
2023-06-19 16:24:49 +0200

    Routes modified, calendar for added to finalize page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_19_085727) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_19_135047) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -69,8 +69,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_19_085727) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float "latitude"
-    t.float "longitude"
   end
 
   create_table "identifications", force: :cascade do |t|
@@ -116,6 +114,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_19_085727) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "move_in_date"
     t.index ["property_id"], name: "index_property_applications_on_property_id"
     t.index ["user_id"], name: "index_property_applications_on_user_id"
   end
